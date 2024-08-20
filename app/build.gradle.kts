008a6b432dfe8032a96896c9d7ec041c0a7e4949
john0 <john024996@gmail.com>
2024-08-19 21:21:56 -0400

    Setting dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,20 +50,54 @@ android {
 }
 
 dependencies {
-
+    // Base implementations
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material)
     implementation(libs.androidx.material3)
+
+
+    //Multidex
+    implementation(libs.androidx.multidex)
+
+    //System Ui Controller
+    implementation(libs.accompanist.systemuicontroller)
+
+    //test
     testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+
+    //Material icons
+    implementation(libs.compose.material.icons.extended)
+
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    //coil
+    implementation(libs.coil.compose)
+
+    //Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    //Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage.ktx)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
