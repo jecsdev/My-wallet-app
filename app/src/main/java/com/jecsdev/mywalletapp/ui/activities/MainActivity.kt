506903dd085c0199ee4dce7a5487d3b9ab9eae4f
John Campusano <john024996@gmail.com>
2024-08-26 14:33:00 -0400

    implement firebase login with google

--- app/src/main/java/com/jecsdev/mywalletapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/jecsdev/mywalletapp/ui/activities/MainActivity.kt
@@ -5,15 +5,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.jecsdev.mywalletapp.ui.viewmodels.AuthViewModel
 import com.jecsdev.mywalletapp.R
 import com.jecsdev.mywalletapp.presentation.navigation.NavGraph
 import com.jecsdev.mywalletapp.ui.theme.MyWalletAppTheme
+import com.jecsdev.mywalletapp.ui.theme.phantomGrayColor
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,12 +36,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = colorResource(R.color.phantom_gray_color)
                 ) {
-                    val navController = rememberNavController()
-                    NavGraph(
-                        context = this@MainActivity,
-                        navController = navController,
-                        authViewModel = authViewModel
-                    )
+                    val systemUiController = rememberSystemUiController()
+                    systemUiController.setStatusBarColor(phantomGrayColor)
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .systemBarsPadding()
+                            .padding(top = 12.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        val navController = rememberNavController()
+                        NavGraph(
+                            context = this@MainActivity,
+                            navController = navController,
+                            authViewModel = authViewModel
+                        )
+                    }
                 }
             }
         }
